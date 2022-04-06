c18663940681de0d77030648a16c4048574da09c
Aisylu24 <firnatovna@mail.ru>
2022-04-06 17:31:45 +0300

    14

--- src/Components/Profile.tsx
+++ src/Components/Profile.tsx
@@ -1,10 +1,10 @@
 import React from "react";
+import classes from './Profile.module.css'
 
 
-
-const Profile = ()=> {
+const Profile = () => {
     return (
-        <div className='content'>
+        <div className={classes.content}>
             <div>
                 <img
                     src="https://sun9-84.userapi.com/impf/RpcHzTaac5bDQr0ACHUqBtU3ONR-eQWM_4jLlg/Pk0lR7iGNeo.jpg?size=2560x1707&quality=95&sign=4379bcada20c7bb17e07cd8ec5f0d294&type=album"
@@ -12,10 +12,14 @@ const Profile = ()=> {
             </div>
             <div>ava+description</div>
             <div>
-                My posts
-                <div>New post</div>
-                <div>post 1</div>
-                <div>post 2</div>
+                    My posts
+                <div>
+                    New post
+                </div>
+                <div className={classes.posts}>
+                <div className={classes.item}>post 1</div>
+                <div className={classes.item}>post 2</div>
+                </div>
             </div>
         </div>
     )
