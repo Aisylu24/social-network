d63c6cecca1c303140d86d72b3ab4d2fd2d03427
Aisylu24 <firnatovna@mail.ru>
2022-04-06 15:34:24 +0300

    13

---
+++ src/Components/Profile.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+
+
+const Profile = ()=> {
+    return (
+        <div className='content'>
+            <div>
+                <img
+                    src="https://sun9-84.userapi.com/impf/RpcHzTaac5bDQr0ACHUqBtU3ONR-eQWM_4jLlg/Pk0lR7iGNeo.jpg?size=2560x1707&quality=95&sign=4379bcada20c7bb17e07cd8ec5f0d294&type=album"
+                    alt=""/>
+            </div>
+            <div>ava+description</div>
+            <div>
+                My posts
+                <div>New post</div>
+                <div>post 1</div>
+                <div>post 2</div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile
