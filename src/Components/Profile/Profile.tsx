1e05f80ebe417cf989e9fd05279bbc339f202611
Aisylu24 <firnatovna@mail.ru>
2022-04-23 18:46:41 +0300

    18

--- src/Components/Profile/Profile.tsx
+++ src/Components/Profile/Profile.tsx
@@ -5,9 +5,11 @@ import MyPosts from "./MyPosts/MyPosts";
 
 const Profile = () => {
     return (
-        <div className={classes.content}>
+        <div
+            // className={classes.content}
+        >
              <div>
-                 <img
+                 <img className={classes.img}
                     src="https://sun9-70.userapi.com/impf/WcawhtE_gFfDESKh-l8rtClDjgoSfOYnjrFAIA/cDMzHGeSmoo.jpg?size=2560x1260&quality=95&sign=4fb12f322f8738ec25c52609749a694f&type=album"
                      alt=""/>
              </div>
