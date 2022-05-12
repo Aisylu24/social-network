210c6edf63f03137b7fc373ad4cd0fe0f2b55d24
Aisylu24 <firnatovna@mail.ru>
2022-05-12 13:30:34 +0300

    21

--- src/Components/Dialogs/Dialogs.tsx
+++ src/Components/Dialogs/Dialogs.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
-
+import s from './Dialogs.module.css'
 
 
 const Dialogs = () => {
     return (
-        <div>Dialogs</div>
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active} >
+                    Zarina
+                </div>
+                <div className={s.dialog}>
+                    Guzel
+                </div>
+                <div className={s.dialog}>
+                    Liza
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>What's up?</div>
+                <div className={s.message}>Are you ok?</div>
+            </div>
+        </div>
     );
 };
 
