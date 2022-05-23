59eae3c6d545f03a09e40c2adffb44f7d346a078
Aisylu24 <firnatovna@mail.ru>
2022-05-23 21:23:13 +0300

    34 removed useRef

--- src/Components/Profile/MyPosts/MyPosts.tsx
+++ src/Components/Profile/MyPosts/MyPosts.tsx
@@ -1,4 +1,4 @@
-import React, {useRef} from "react";
+import React, {ChangeEvent, useRef} from "react";
 import s from './MyPosts.module.css'
 import Post from "./Post/Post";
 import {PostType} from "../../../redux/state";
@@ -16,25 +16,20 @@ const MyPosts = (props: MyPostsPropsType) => {
 
     let postElements = props.posts.map(p => <Post message={p.message} likes={p.likesCount}/>)
 
-    let newPostElement = useRef<HTMLTextAreaElement>(null)
-
     const addPost = () => {
-        if (newPostElement?.current?.value) {
             props.addPost()
-        }
     }
 
-    const onPostChange = () => {
-        if (newPostElement?.current?.value) {
-        props.updateNewPostText(newPostElement.current.value)
-    }}
+    const onPostChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
+        props.updateNewPostText(event.currentTarget.value)
+    }
 
     return (
         <div className={s.postsBlock}>
             <h4>My posts</h4>
             <div>
                 <div>
-                    <textarea ref={newPostElement}
+                    <textarea
                               value={props.newPostText}
                               onChange={onPostChange}
                     />
