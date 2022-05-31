8bf453b06fecc648f10f3e8cdcab90a3a0197f54
Aisylu24 <firnatovna@mail.ru>
2022-05-31 12:36:20 +0300

    44 - typing

--- src/StoreContext.tsx
+++ src/StoreContext.tsx
@@ -4,13 +4,13 @@ import {StoreType} from "./redux/store";
 
 export const StoreContext = React.createContext({} as StoreType)
 
+export type ProviderType = {
+    store: StoreType
+    children: React.ReactNode
+}
 
-export const Provider = (props: {
-    store: StoreType;
-    children: boolean
-        | React.ReactChild | React.ReactFragment | React.ReactPortal
-        | null | undefined;
-}) => {
+
+export const Provider = (props: ProviderType) => {
     return (
         <StoreContext.Provider value={props.store}>
             {props.children}
