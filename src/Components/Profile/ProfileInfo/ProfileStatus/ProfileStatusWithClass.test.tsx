d40b740afed7142324dc2d6e84aa7df3f7ccdc4a
Aisylu24 <firnatovna@mail.ru>
2022-12-12 19:58:48 +0300

    92 - react-test-renderer

---
+++ src/Components/Profile/ProfileInfo/ProfileStatus/ProfileStatusWithClass.test.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import {create} from "react-test-renderer";
+import {ProfileStatusWithClass} from "./ProfileStatusWithClass";
+
+describe("ProfileStatusWithClass component", () => {
+    test("status from props should be in the state", () => {
+        const component = create(<ProfileStatusWithClass status={'Aisylu'} updateUserStatus={() => {
+        }}/>);
+        const instance = component.getInstance();
+        expect(instance.state.status).toBe("Aisylu");
+    });
+    test("span should be shown", () => {
+        const component = create(<ProfileStatusWithClass status={'Aisylu'} updateUserStatus={() => {
+        }}/>);
+        const instance = component.root;
+        let span = instance.findByType("span")
+        expect(span).not.toBeNull();
+    });
+    test("span should be shown like...", () => {
+        const component = create(<ProfileStatusWithClass status={'Aisylu'} updateUserStatus={() => {
+        }}/>);
+        const instance = component.root;
+        let span = instance.findByType("span")
+        expect(span.children[0]).toBe('Aisylu');
+    });
+    test("input shouldn't be shown", () => {
+        const component = create(<ProfileStatusWithClass status={'Aisylu'} updateUserStatus={() => {
+        }}/>);
+        const instance = component.root;
+        expect(() => {
+            instance.findByType("input")
+        }).toThrow();
+    });
+    test("input shouldn be shown in editMode instead of span", () => {
+        const component = create(<ProfileStatusWithClass status={'Aisylu'} updateUserStatus={() => {
+        }}/>);
+        const instance = component.root;
+        let span = instance.findByType("span")
+        span.props.onDoubleClick()
+        let input = instance.findByType("input")
+        expect(input.props.value).toBe('Aisylu');
+    });
+    test("callback should be called", () => {
+        let callback = jest.fn()
+        const component = create(<ProfileStatusWithClass status={'Aisylu'} updateUserStatus={callback}/>);
+        const instance = component.getInstance()
+        instance.deactivateEditMode()
+        // expect(callback.mock.calls[0][0]).toBe('Aisylu');
+        expect(callback).toHaveBeenCalledWith('Aisylu');
+        // expect(callback.mock.calls.length).toBe(1);
+        expect(callback).toHaveBeenCalledTimes(1)
+    });
+});
